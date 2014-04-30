f6cfb6567794f0e191e42a6c0acd2f2ac1851d44
Sejin Park <apikoka@me.com>
2014-04-30 17:37:37 +0900

    pi calculating function made

--- main.cpp
+++ main.cpp
@@ -3,25 +3,37 @@
 #include <iomanip>
 using namespace std;
 
+void pi_count(double *total_count, double *incount);
+
 int main()
 {
 	cout.setf(ios::fixed);
 	cout.precision(5);
 	double incount=0;
 	double total=0;
-	for(double i=-1;i<=1;i+=0.01)
+
+	pi_count(&total, &incount);
+
+	cout<<"total: "<<total<<"\tin: "<<incount<<endl;;
+
+}
+
+
+
+void pi_count(double *total_count, double *incount)
+{
+	cout<<*total_count<<*incount<<endl;
+	for(double i=-1;i<=1;i+=0.1)
 	{
-		for(double j=-1;j<=1;j+=0.01)	
+		for(double j=-1;j<=1;j+=0.1)	
 		{
-			total++;
+			(*total_count)++;
 
 			if((i*i+j*j)<=1)
 			{
-				incount++;
+				(*incount)++;
 			}
 		}
 	}
-
-	cout<<"total: "<<total<<"\tin: "<<incount<<endl;;
 
 }
